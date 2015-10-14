af7e08eea2327b75e93db80c334b5265361dcbd6
Mei Gwilym <mei.gwilym@gmail.com>
2015-10-14 22:37:55 +0100

    new routes for app; auth routes; edit auth middleware with new login route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,36 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return redirect()->route('admin');
 });
+
+// Authentication routes...
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@getLogout');
+
+Route::group(['middleware' => 'auth', 'prefix' => 'admin', 'namespace' => 'Admin'], function()
+{
+    // admin dashboard
+    Route::get('/', ['as' => 'admin', 'use' => 'AdminController@index']); 
+
+    // manage members & subscriptions
+    Route::get('members', ['as' => 'admin.members', 'use' => 'MemberController@index']);
+    Route::get('members/create', ['as' => 'admin.members.create', 'use' => 'MemberController@create']);
+    Route::post('members/store', ['as' => 'admin.members.store', 'use' => 'MemberController@store']);
+    Route::get('members/{id}', ['as' => 'admin.members.show', 'use' => 'MemberController@show']);
+    Route::get('members/{id}/edit', ['as' => 'admin.members.edit', 'use' => 'MemberController@edit']);
+    Route::put('members/{id}/update', ['as' => 'admin.members.update', 'use' => 'MemberController@update']);
+
+    // manage memberships
+    Route::get('memberships', ['as' => 'admin.memberships', 'use' => 'MembershipController@index']);
+    Route::get('memberships/create', ['as' => 'admin.memberships.create', 'use' => 'MembershipController@']);
+    Route:post('memberships/store', ['as' => 'admin.memberships.store', 'use' => 'MembershipController@store']);
+    Route::get('memberships/{id}', ['as' => 'admin.memberships.show', 'use' => 'MembershipController@show']);
+    Route::get('memberships/{id}/edit', ['as' => 'admin.memberships.edit', 'use' => 'MembershipController@edit']);
+    Route::get('memberships/{id}/update', ['as' => 'admin.memberships.update', 'use' => 'MembershipController@update']);
+    
+});
+
+
