da20986d61834f98fd1b3b8727bc292c6112b921
Mei Gwilym <mei.gwilym@gmail.com>
2015-10-15 21:35:42 +0100

    proper working of Season model

--- app/Season.php
+++ app/Season.php
@@ -12,30 +12,46 @@ namespace App;
  */
 class Season
 {
-  public static $name;
-  public static $starts;
-  public static $ends;
-  
-  public function __construct()
-  {
-    if(self::$name === null)
-    {      
-      // from Jul - Dec
-      if(date('m') > 6)
-      {
-          self::$name = date('y').'-'.(date('y')+1);
-          
-          self::$starts = new DateTime(date('y').'-7-1'); // 1st july
-          self::$ends = new DateTime((date('y')+1).'-6-30 23:59:59'); // end of june next year
-      }
-      // from Jan - Jun
-      else
-      {
-          self::$name = (date('y')-1).'-'.date('y'); 
-          
-          self::$starts = new DateTime((date('Y')-1).'-7-1'); 
-          self::$ends = new DateTime((date('y')).'-6-30 23:59:59'); 
-      }
+    public static $name;
+    public static $starts;
+    public static $ends;
+
+    public static function init()
+    {
+        if (!self::$name)
+        {
+            // from Jul - Dec
+            if (date('m') > 6) {
+                self::$name = date('y').'-'.(date('y') + 1);
+
+                self::$starts = new \DateTime(date('y').'-7-1'); // 1st july
+                self::$ends   = new \DateTime((date('y') + 1).'-6-30 23:59:59'); // end of june next year
+            }
+            // from Jan - Jun
+            else {
+                self::$name = (date('y') - 1).'-'.date('y');
+
+                self::$starts = new \DateTime((date('Y') - 1).'-7-1');
+                self::$ends   = new \DateTime((date('y')).'-6-30 23:59:59');
+            }
+        }
     }
-  }
+
+    public static function name()
+    {
+        self::init();
+        return self::$name;
+    }
+
+    public static function starts()
+    {
+        self::init();
+        return self::$starts;
+    }
+
+    public static function ends()
+    {
+        self::init();
+        return self::$ends;
+    }
 }
